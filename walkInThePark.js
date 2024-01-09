aacea56355a3e0786f95559944c1bb04151f3bc9
Suyeon <clapforkite@gmail.com>
2024-01-09 22:22:38 +0900

    update: walk in park

--- walkInThePark.js
+++ walkInThePark.js
@@ -6,17 +6,53 @@ function solution(park, routes) {
     answer = [0, answer];
     for (let i = 0; i < routes.length; i++) {
         const data = routes[i].split(" ");
-        if (data[0] == "E" && park[answer[0]][Number(data[1]) + Number(answer[1])] == 'O') {
-            answer[1] = Number(data[1]) + Number(answer[1]);
+        data[1] = Number(data[1]);
+        let isGoing = true;
+
+        let a = 0;
+        let b = 0;
+
+        if (data[0] == "N" || data[0] == "S") {
+            b = answer[0];
+        } else {
+            b = answer[1];
         }
-        else if (data[0] == "N" && park[Number(answer[0]) - Number(data[1])][answer[1]] == 'O') {
-            answer[0] = Number(answer[0]) - Number(data[1])
+
+        for (let j = b; a <= data[1] && b < park.length && b >= 0; a++) {
+            if (data[0] == "E" && park[answer[0]][j] == 'O') {
+                j++;
+            }
+            else if (data[0] == "N" && park[j][answer[1]] == 'O') {
+                j--;
+            }
+            else if (data[0] == "W" && park[answer[0]][j] == 'O') {
+                j++
+            } else if (data[0] == "S" && park[j][answer[1]] == 'O') {
+                j--;
+            } else {
+                isGoing = false;
+                break;
+            }
         }
-        else if (data[0] == "W" && park[answer[0]][Number(answer[1]) - Number(data[1])] == 'O') {
-            answer[1] = Number(answer[1]) - Number(data[1]);
-        } else if (data[0] == "S" && park[Number(answer[0]) + Number(data[1])][answer[1]] == 'O') {
-            answer[0] = Number(answer[0]) + Number(data[1])
+
+        if (isGoing) {
+            if (data[0] == "E") {
+                answer[1] = Number(data[1]) + Number(answer[1]);
+                j++;
+            }
+            else if (data[0] == "N") {
+                answer[0] = Number(answer[0]) - Number(data[1])
+                j--;
+            }
+            else if (data[0] == "W") {
+                answer[1] = Number(answer[1]) - Number(data[1]);
+                j++
+            } else if (data[0] == "S") {
+                answer[0] = Number(answer[0]) + Number(data[1])
+                j--;
+            }
         }
+
         console.log(answer)
     }
     return answer;
