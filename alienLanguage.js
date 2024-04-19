5de2ac75d32831206b1c7f219f23d10c012d94d0
Suyeon <s2026@e-mirim.hs.kr>
2024-04-19 17:41:09 +0900

    repactor : 외계인 사전

--- alienLanguage.js
+++ alienLanguage.js
@@ -1,21 +1,13 @@
 function solution(spell, dic) {
-    for (let data of dic) {
-        let answer = 1;
+    for(let data of dic){
         let checkDic = new Array(spell.length).fill(0);
-        for (let dics of data.split("")) {
-            if (spell.includes(dics)) {
+        for(let dics of data.split("")){
+            if(spell.includes(dics)){
                 checkDic[spell.indexOf(dics)]++;
-            }
+            } 
         }
-        console.log(checkDic.filter(data => data != 1))
-        for (let checkData of checkDic) {
-            if (checkData !== 1) {
-                answer = 2;
-                break;
-            }
-        }
-        if (answer == 1) {
-            return answer;
+        if(checkDic.findIndex(data => data !== 1)===-1){
+            return 1;
         }
     }
     return 2;
