0d3f1867ee90520262ae5e11ca2d2ee7f0794096
Suyeon <s2026@e-mirim.hs.kr>
2024-04-20 23:57:37 +0900

    feat: 연속된 수의 합

---
+++ consecutiveNumbers.js
@@ -0,0 +1,6 @@
+function solution(num, total) {
+    let answer = new Array(num).fill(0);
+    const middleNum = Math.floor(total / num);
+    const remainNum = total % num;
+    for (let i = middleNum /; i <)
+}
