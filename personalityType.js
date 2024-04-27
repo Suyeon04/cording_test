5ae08d9a88b7a67201fbe428c524af9838ef229b
Suyeon <s2026@e-mirim.hs.kr>
2024-04-27 22:17:29 +0900

    feat: 성격 유형 검사하기

---
+++ personalityType.js
@@ -0,0 +1,18 @@
+function solution(survey, choices) {
+  var answer = "";
+  const scoreData = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+  for (let i = 0; i < survey.length; i++) {
+    const surveyData = survey[i].split("");
+    if (choices[i] < 4) {
+      scoreData[surveyData[0]] += 4 - choices[i];
+    } else {
+      scoreData[surveyData[1]] += choices[i] - 4;
+    }
+  }
+  answer =
+    (scoreData["R"] < scoreData["T"] ? "T" : "R") +
+    (scoreData["C"] < scoreData["F"] ? "F" : "C") +
+    (scoreData["J"] < scoreData["M"] ? "M" : "J") +
+    (scoreData["A"] < scoreData["N"] ? "N" : "A");
+  return answer;
+}
