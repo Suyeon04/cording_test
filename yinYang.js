d3245e07945ff5ed2a5e0623b3538f68b236438e
Suyeon <s2026@e-mirim.hs.kr>
2024-05-08 23:57:04 +0900

    feat: 음양 더하기

---
+++ yinYang.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+  var answer = 0;
+  for (let i = 0; i < absolutes.length; i++) {
+    if (!signs[i]) {
+      answer += -1 * absolutes[i];
+    } else {
+      answer += absolutes[i];
+    }
+  }
+  return answer;
+}
