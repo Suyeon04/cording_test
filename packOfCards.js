801b47ce1eaeb66e4301f4827ee783bc1dc0bed2
Suyeon <s2026@e-mirim.hs.kr>
2024-05-10 00:00:13 +0900

    repactor : 카드뭉치

--- packOfCards.js
+++ packOfCards.js
@@ -1,14 +1,14 @@
 function solution(cards1, cards2, goal) {
-  let cards1Length = 0;
-  let cards2Length = 0;
-  for (let i = 0; i < goal.length; i++) {
-    if (goal[i] == cards1[cards1Length]) {
-      cards1Length++;
-    } else if (goal[i] == cards2[cards2Length]) {
-      cards2Length++;
-    } else {
-      return "No";
+    let cards1Length = 0;
+    let cards2Length = 0;
+    for(let i of goal){
+        if(i == cards1[cards1Length]){
+            cards1Length++;
+        }else if(i == cards2[cards2Length]){
+            cards2Length++;
+        }else{
+           return 'No' ;
+        }
     }
-  }
-  return "Yes";
+    return 'Yes';
 }
