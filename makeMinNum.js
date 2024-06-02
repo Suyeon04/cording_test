369924d5eef8235ad8f763abaf7877fbd8b01e9d
Suyeon <s2026@e-mirim.hs.kr>
2024-06-02 21:58:29 +0900

    feat : 가능한 가장 작은 두 숫자 만들기

---
+++ makeMinNum.js
@@ -0,0 +1,41 @@
+function test(data) {
+  data = data.split(", ");
+  data.sort((a, b) => a - b);
+
+  if (data[0] == 0 || data[1] == 0) {
+    if (data.length > 3) {
+      let changeIndex = 1;
+      while (changeIndex < data.length) {
+        if (data[0] == 0) {
+          data[0] = data[changeIndex];
+          data[changeIndex] = 0;
+          changeIndex++;
+          if (data[1] == 0) {
+            data[1] = data[changeIndex];
+            data[changeIndex] = 0;
+            changeIndex++;
+          }
+        } else {
+          break;
+        }
+      }
+      if (data[0] == 0 || data[1] == 0) {
+        return -1;
+      } else {
+        return -1;
+      }
+    }
+  }
+
+  let firstNum = [];
+  let secondNum = [];
+  for (let i = 0; i < data.length; i++) {
+    if (i % 2 == 0) {
+      firstNum.push(data[i]);
+    } else {
+      secondNum.push(data[i]);
+    }
+  }
+
+  return parseInt(firstNum.join("")) + parseInt(secondNum.join(""));
+}
