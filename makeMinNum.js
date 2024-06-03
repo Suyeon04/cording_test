128b5cde4eefc11491eb4241b73c663f917be391
Suyeon <s2026@e-mirim.hs.kr>
2024-06-03 23:34:10 +0900

    fix : 가능한 가장 작은 두 숫자 만들기

--- makeMinNum.js
+++ makeMinNum.js
@@ -1,41 +1,39 @@
 function test(data) {
-  data = data.split(", ");
+  data = data.split(", ").map(Number);
   data.sort((a, b) => a - b);
 
-  if (data[0] == 0 || data[1] == 0) {
+  if (data[0] === 0 || data[1] === 0) {
     if (data.length > 3) {
       let changeIndex = 1;
       while (changeIndex < data.length) {
-        if (data[0] == 0) {
-          data[0] = data[changeIndex];
-          data[changeIndex] = 0;
+        if (data[0] === 0) {
+          [data[0], data[changeIndex]] = [data[changeIndex], data[0]];
           changeIndex++;
-          if (data[1] == 0) {
-            data[1] = data[changeIndex];
-            data[changeIndex] = 0;
+          if (data[1] === 0 && changeIndex < data.length) {
+            [data[1], data[changeIndex]] = [data[changeIndex], data[1]];
             changeIndex++;
           }
         } else {
           break;
         }
       }
-      if (data[0] == 0 || data[1] == 0) {
-        return -1;
-      } else {
+      if (data[0] === 0 || data[1] === 0) {
         return -1;
       }
+    } else {
+      return -1;
     }
   }
 
   let firstNum = [];
   let secondNum = [];
+
   for (let i = 0; i < data.length; i++) {
-    if (i % 2 == 0) {
+    if (i % 2 === 0) {
       firstNum.push(data[i]);
     } else {
       secondNum.push(data[i]);
     }
   }
-
   return parseInt(firstNum.join("")) + parseInt(secondNum.join(""));
 }
