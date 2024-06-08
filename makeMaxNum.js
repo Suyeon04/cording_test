e80d28e40b1fb0f48016bbf2847935b695852346
Suyeon <s2026@e-mirim.hs.kr>
2024-06-09 00:55:10 +0900

    feat : 가장 큰 숫자 만들기

--- makeMaxNum.js
+++ makeMaxNum.js
@@ -1,16 +1,34 @@
 function solution(N, K) {
-  const splitN = String(N).split("");
   let result = [];
   let isPush = false;
-  for (let i = 0; i < splitN.length; i++) {
-    if (i < K && !isPush) {
-      result.push(K);
-      isPush = true;
+  const isEssence = N >= 0 ? true : false;
+  if (isEssence) {
+    const splitN = String(N).split("");
+    for (let i = 0; i < splitN.length; i++) {
+      if (i < K && !isPush) {
+        result.push(K);
+        isPush = true;
+      }
+      result.push(splitN[i]);
+      if (i == splitN.length - 1 && !isPush) {
+        result.push(K);
+      }
     }
-    result.push(splitN[i]);
-    if (i == splitN.length - 1 && !isPush) {
+    return Number(result.join(""));
+  } else {
+    N *= -1;
+    while (N != 0) {
+      let num = N % 10;
+      if (!isPush && num < K) {
+        isPush = true;
+        result.push(K);
+      }
+      result.push(num);
+      N = parseInt(N / 10);
+    }
+    if (!isPush) {
       result.push(K);
     }
+    return Number(result.reverse().join("")) * -1;
   }
-  return Number(result.join(""));
 }
